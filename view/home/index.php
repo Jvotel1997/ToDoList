691e10e82ab1eaba8913e8632acc310282cd079d
Unknown <jvotel@live.nl>
2018-04-11 11:57:34 +0200

    Added style.

--- view/home/index.php
+++ view/home/index.php
@@ -2,6 +2,10 @@
   <?php foreach ($lists as $list) { ?>
     <div class="list">
       <div class="list-name"><?= $list['list_name']; ?></div>
+      <div class="sort">
+        <span>Naam</span>
+        <a href=""class="sort-status">Status</a>
+      </div>
       <div class="task-items">
         <?php foreach (getAllTasksFromList($list['list_id']) as $task) { ?>
           <div class="task">
@@ -9,17 +13,24 @@
               <span>
                 <?= $task['task_description']; ?>
               </span>
+              <span class="status">
+                <?= $task['status']; ?>
+              </span>
+              <span class="task-menu">
                 <a href="<?= URL ?>task/delete/<?= $task['task_id'] ?>">Delete</a>
                 <a href="<?= URL ?>task/edit/<?= $task['task_id'] ?>">Edit</a>
+              </span>
             </p>
           </div>
         <?php } ?>
       </div>
+      <div class="options">
         <a href="<?= URL ?>task/create/<?= $list['list_id'] ?>" id="add-task">Add a task</a>
         <div>
           <a href="<?= URL ?>list/delete/<?= $list['list_id'] ?>">Delete list</a>
           <a href="<?= URL ?>list/edit/<?= $list['list_id'] ?>">Edit list</a>
         </div>
+      </div>
     </div>
   <?php } ?>
   <a href="<?= URL ?>list/create" id="add-list">Add a list...</a>
--- view/templates/header.php
+++ view/templates/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="<?= URL ?>css/custom.css">
 	<title>Todolist</title>
 </head>
 <body>
